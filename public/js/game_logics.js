aeaf564ed688e91af5986bc065dd5ac9f360b51e
praneetheus <pellanti@sfu.ca>
2019-07-15 19:12:03 -0700

    buy/sell is functional now

--- public/js/game_logics.js
+++ public/js/game_logics.js
@@ -1,18 +1,3 @@
-const buyButton = document.getElementById('buyButton');
-const sellButton = document.getElementById('sellButton');
-
-buyButton.addEventListener('click', function(e){
-  //buy funcion
-  // console.log('button was clicked');
-//   alert("Unable to buy the property");
-    confirm("Are you sure you want to buy this property?");
-});
-
-sellButton.addEventListener('click', function(e){
-  //sell funcion
-});
-
-
 // adding messages to the game log
 function addToGameLog(message)
 {
@@ -151,9 +136,11 @@ function Property(name, pricetext, color, price, groupNumber, baserent, level1, 
 	}
 }
 
+// making property a global variable so it can be used to buy/sell/trade
+var property = [];
 // set up the board game
 function set_up_game_board(){
-    var property = [];
+    // var property = [];
     
     //initialize properties on the board
     property[0] = new Property("Start", "COLLECT $200 TRAVEL SUBSIDY AS YOU PASS.", "#FFFFFF");
@@ -297,6 +284,169 @@ function set_up_game_board(){
 //	});
 }
 
+
+class Player {
+    constructor(name, picture, playerNumber) {
+      this.name = name;
+      this.cash = 1500;
+      this.picture = picture;
+      this.curCell = 0;
+      this.playerNumber = playerNumber;
+      // what other attributes we need?
+    }
+  
+    updatePosition(stepsToMove) {
+        // get current player location
+        let currentPlayerPosition = this.curCell;
+        // get newPosition after roll
+        let newPositionAfterRoll = (currentPlayerPosition + stepsToMove); 
+        //update cash if the player completes one lap around the board
+        if (newPositionAfterRoll >= boardLength) { //makes full revolution
+          this.cash += 200;
+          addToGameLog('You made it around the board! Collect $200!');
+        }
+        // update player current position
+        return (newPositionAfterRoll % boardLength);
+      }
+    
+      movePlayer() {
+        // get the cell numbers
+        var oldCellNum = this.curCell;
+        var newCellNum = this.updatePosition();
+    
+        // get the innerHTML of the table inside the cell
+        var pic = document.getElementById('cell'+m+'grid'+this.playerNumber).innerHTML;
+    
+        // move the innerHTML of the table inside the cell to the same table in the next cell
+        var m = oldCellNum;
+    
+        var interval = setInterval(() => {
+          // Re-enable the button
+          if (m == newCellNum) {
+            document.getElementById("throw").disabled = false;
+          }
+          // Move character one square at a time from old position to new position
+          if (lap == false) {
+            if ((m % boardLength) < newCellNum) {
+                document.getElementById('cell'+m+'grid'+0).innerHTML = '';
+                m = ((m+1) % boardLength);
+                document.getElementById('cell'+m+'grid'+0).innerHTML = pic;
+            } else {
+                oldCellNum = newCellNum;
+                clearInterval(int);
+            }
+          } else {
+              if ((m % boardLength) < newCellNum) {
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = '';
+                  m = ((m+1) % boardLength);
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = pic;
+                  lap = false;
+              } else if (m == boardLength-1) { 
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = '';
+                  m = 0; 
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = pic;
+                  lap = false;
+              } else if (m < boardLength) {
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = '';
+                  m = ((m+1) % boardLength);
+                  document.getElementById('cell'+m+'grid'+0).innerHTML = pic;
+              } else {
+                  lap = false;
+                  oldCellNum = newCellNum;
+                  clearInterval(int);
+              }
+            }
+          }, 500);
+
+        this.curCell = newCellNum;
+      }
+    
+    // logic not complete
+    buyProperty(square) {
+        console.log("player cash before purchase is: " + this.cash);
+        if (square.owner === 0) {
+        // update the square owner square to this player
+            square.owner = this;
+        // subtract the cash from player
+            this.cash -= square.price; 
+        // else if this is already owned by another player
+        // continue
+            console.log("player cash after purchase is: " + this.cash);
+            addToGameLog(this.name + " has bought " + square.name);
+        } else {
+            alert("Unable to buy the property");
+        }
+    }
+  
+    sellProperty(square) {
+      //check if this player owns the square
+      if (square.owner === this) {
+        // update the owner to null/no one
+        square.owner = 0; 
+        // add half the cash to player
+        this.cash += (square.price)*0.50; 
+        // else if this is already owned by another player
+        // continue
+        addToGameLog(this.name + " has sold " + square.name);
+      } else {
+        alert("Unable to sell the property");
+      }
+    }
+  
+    getPlayerPosition() {
+      return this.curCell;
+    }
+  }
+
+  // creating a player
+  let p1 = new Player("pee", "pic", 1);
+  
+  // returning the current player, this code needs to be changed to be more dynamic
+  function getCurrentPlayer() {
+    return p1; // how to do this?
+  }
+
+  const buyButton = document.getElementById('buyButton');
+  const sellButton = document.getElementById('sellButton');
+  
+  buyButton.addEventListener('click', function(e){
+      var currentPlayer = getCurrentPlayer();
+    //   console.log("current cell is: " + currentPlayer.curCell);
+      var playerPosition = currentPlayer.curCell; // should return int
+      var currentSquare = property[2]; //this will get the property
+    //   console.log("squre name is: " + currentSquare.name);
+      // already owned
+      if (currentSquare.owner != 0) {
+          alert("This property is already owned");
+      } 
+      //make sure they have enough cash
+      if (currentPlayer.cash >= currentSquare.price) {
+          var confirmed = confirm("Are you sure you want to buy this property?");
+          if (confirmed) {
+              currentPlayer.buyProperty(currentSquare);
+          }
+      } else {
+          alert("You do not have the funds to buy the property"); //add to gamelog
+          addToGameLog("You do not have the funds to buy the property");
+      }
+     
+  });
+  
+  sellButton.addEventListener('click', function(e){
+    var currentPlayer = getCurrentPlayer();
+    
+    var playerPosition = currentPlayer.curCell; 
+    var currentSquare = property[2]; 
+  //   console.log("squre name is: " + currentSquare.name);
+
+    if (currentSquare.owner != currentPlayer) {
+        alert("This property is not owned by you");
+    }
+    currentPlayer.sellProperty(currentSquare); 
+    //make sure they have enough cash
+    console.log("player cash after selling: " + currentPlayer.cash);
+  });
+  
 
 window.onload = function() {
     set_up_game_board()
