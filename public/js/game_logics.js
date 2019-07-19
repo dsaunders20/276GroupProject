9ee8703c891a67284ffde430d2f74a946a78a899
D Saunders <devinsaunders709@gmail.com>
2019-07-18 21:06:31 -0700

    testing bing

--- public/js/game_logics.js
+++ public/js/game_logics.js
@@ -442,7 +442,7 @@ window.onload = function () {
     set_up_game_board()
     
     // retrieve current weather and display
-    //getWeather();
+    bingImageSearch('granville island');
     
     //retrieve players info
     $.ajax({
