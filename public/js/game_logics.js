6923ea7a91de6660ab8f42d04e1a50e69d5cd44d
Stanley Kwok <stnly.kwk@gmail.com>
2019-07-29 13:32:19 -0700

    Removed duplicate lines

--- public/js/game_logics.js
+++ public/js/game_logics.js
@@ -144,7 +144,7 @@ function updateDice() {
 // ----------------------------------- DICE ROLLING ------------------------------------------
 
 
-// ----------------------------------- JAIL LOGIC ------------------------------------------
+// ----------------------------------- JAIL FUNCTIONS ------------------------------------------
 
 const jailButton = document.getElementById("leaveJail");
 jailButton.addEventListener('click', function(b){
@@ -181,34 +181,8 @@ function unJail(player) {
     jailButton.disabled = true;
 }
 
-// ----------------------------------- JAIL LOGIC ------------------------------------------
+// ----------------------------------- JAIL FUNCTIONS ------------------------------------------
 
-
-// ----------------------------------- JAIL LOGIC ------------------------------------------
-
-//  Send player to jail and update attributes
-function toJail(player) {
-    //  Move avatar
-    var character_img = document.createElement("img");
-        character_img.src = "/images/" + player.picture + "character.png";
-        character_img.setAttribute("height", "auto");
-        character_img.setAttribute("width", "25%");
-        character_img.setAttribute("padding-top", "10px");
-    document.getElementById('cell'+ player.curCell + 'positionholder').innerHTML = '';  //  Maybe use some animations?
-    document.getElementById('cell10positionholder').appendChild(character_img);         //  But not sure how to implement...
-
-    //  Set jail attributes
-    player.inJail = true;
-    player.curCell = 10;
-    player.turnsInJail = 0;
-}
-
-function unJail(player) {
-    player.inJail = false;
-    jailButton.disabled = true;
-}
-
-// ----------------------------------- JAIL LOGIC ------------------------------------------
 
 // ----------------------------------- Properties ------------------------------------------
 function Property(name, pricetext, color, price, groupNumber, baserent, level1, level2, level3, level4) {
